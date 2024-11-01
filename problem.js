f5b25d68d28c08eaa0e211a9d8704e447d1a67a5
Zinna <skim661@my.bcit.ca>
2024-11-01 14:55:03 -0700

    Update problem.js

--- problem.js
+++ problem.js
@@ -13,3 +13,36 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+function passwordChecker(userInput, actualPassword){
+    // password checker
+    if(userInput === actualPassword){ 
+        if(userInput==="forgot"|| userInput==="reset"){ //challenge
+            console.log("Access Granted! This password you set should not be used because it glitches the system");
+        } else {
+            console.log("Access Granted!");
+        }
+
+    } else if(userInput === "forgot"){
+        console.log("Here is a hint");
+
+    } else if(userInput === "reset"){
+        console.log("Let's reset your account");
+
+    } else if(userInput !== actualPassword){
+        console.log("Access Denied!");
+    }
+    
+    //challenge, length output
+    if(userInput.length < 5){
+        console.log("Your password is too short!");
+    }
+    
+}
+
+passwordChecker("goahdsifkasdf", "goahdsifkasdf"); // access granted
+passwordChecker("good", "greate"); // denied, too short
+passwordChecker("good", "good"); // granted, too short
+passwordChecker("reset", "reset"); // granted, but shouldnt be used
+passwordChecker("forgot", "forgot"); // granted, but shouldnt be used
+passwordChecker("forgot", "heehee"); // here is the hint
+passwordChecker("reset", "heehee"); // resetting ur password
